94874ccfeeebdd70a3e6f130bfc4bb4ed7ebceb4
djepi29 <j3anpi3rr3g3org3s@email.com>
2022-11-17 08:37:45 -0500

    combining assertEqual and eqArrays to compare arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,8 @@
+const assertArraysEqual = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      console.log(`🛑🛑🛑 Assertion failed: ${array1} !== ${array2}`);
+    }
+  }
+  console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+};
