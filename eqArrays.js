8ccbc2310a1745ff5c7bbb25751443186f3767b7
djepi29 <j3anpi3rr3g3org3s@email.com>
2022-11-17 07:07:29 -0500

    comparing arrays

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+const eqArrays = function(array1, array2){
+  for (let i = 0; i < array1.length; i++){
+    if (array1[i] !== array2[i]){
+      // console.log(true)
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+// eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
