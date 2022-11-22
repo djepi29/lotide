2053af9d9728950f283d01b3edd87d7423247383
djepi29 <j3anpi3rr3g3org3s@email.com>
2022-11-21 19:16:45 -0500

    choosing key based on value stated in call back

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+const findKey = (object, cb) => {
+  
+  for (const k in object){
+    if (cb(object[k])) {
+      return k
+    }
+  }
+  return undefined
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3)) // => "noma"
